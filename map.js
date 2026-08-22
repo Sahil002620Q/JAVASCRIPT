3a625ff8e39413489b11affa5f6aceb25ebc2744
Sahil002620Q <smax06388@gmail.com>
2026-08-22 21:59:58 +0530

    map operation for array

---
+++ map.js
@@ -0,0 +1,11 @@
+let numbers = [10, 20, 30, 40, 50];
+
+let doubled = (numbers.map((x) => 2*x ))
+
+console.log(doubled)
+
+let numbers = [5, 10, 15, 20];
+
+let doubled = numbers.map((x) => x*x )
+
+console.log(doubled)
