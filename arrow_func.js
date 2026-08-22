30facabc3fcf7424729e9c26f0222c930e1e7405
Sahil002620Q <smax06388@gmail.com>
2026-08-22 21:59:58 +0530

    arrow function in js

---
+++ arrow_func.js
@@ -0,0 +1,20 @@
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+
+const subtract = (a, b) =>  a - b;
+
+const multiply = (a, b) => a * b;
+
+const subtract = (a, b) =>{
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
