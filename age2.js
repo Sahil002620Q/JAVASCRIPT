69c9ed8d46577946645a5811e851c14824a48caf
Sahil002620Q <smax06388@gmail.com>
2026-08-22 21:59:56 +0530

    learned if else in js same as c/cpp

---
+++ age2.js
@@ -0,0 +1,14 @@
+let name = "Sahil";
+let birthYear = 2006;
+let currentYear = 2026;
+let age = currentYear - birthYear;
+
+console.log(`${name} is ${age} years old.`);
+if(age >= 18)
+{
+    console.log("Adult");
+}
+else
+{
+    console.log("Minor");
+}
