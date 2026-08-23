45638407a13d651cd70af699a264df8c28a15619
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:05:30 +0530

    object in js same as json and dict in python

---
+++ object.js
@@ -0,0 +1,13 @@
+let dict = {
+    name: "sahil",
+    age: 19,
+    collage: "daviet"
+}
+
+console.log(dict["name"])
+console.log(dict["age"])
+dict.collage = "Daviet"
+dict.branch = "CSE AI & ML"
+console.log(dict.collage)
+console.log(dict.branch)
+console.log(dict)
