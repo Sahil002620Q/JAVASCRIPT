ba9db525085a780efb51b6ff52c9d3d4c2df20aa
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:04:09 +0530

    using function for math operations

---
+++ func_sum.js
@@ -0,0 +1,20 @@
+function sum(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+console.log(sum(1,4));
+console.log(subtract(6,1));
+console.log(multiply(5,2));
+console.log(divide(10,2));
