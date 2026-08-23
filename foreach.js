98407c6fedf5a0a644a6518152100deb1b974197
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:04:09 +0530

    foreach loop in js

---
+++ foreach.js
@@ -0,0 +1,11 @@
+// let numbers = [10, 15, 22, 31, 40, 53, 60];
+
+// let greaterThan30 = numbers.filter((x) => x > 30 )
+
+// greaterThan30.forEach(x => console.log(x))
+
+
+let numbers = [5, 10, 15, 20, 25];
+numbers.forEach(x => console.log(x))
+
+//array => map filter foreach
