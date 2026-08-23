e49b26d156553731ab3094f41d04931d7a4e00d3
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:04:11 +0530

    array and array operations in js

---
+++ array.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5,6]
+console.log(arr);     //original arr
+console.log(arr[0]);  //arr at index 0
+console.log(arr.length);
+arr.push(10);
+console.log(arr)  
+arr.pop();
+console.log(arr)  
+
+// console.log(arr[0])  
+// console.log(arr[0])  
+// console.log(arr[0])  
+// console.log(arr[0])  
