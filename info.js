e9b16b30bb99768c9e699685692eca95b21fe716
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:00:05 +0530

    constant key in js , if used with var , later the val of that var cannot be modified

---
+++ info.js
@@ -0,0 +1,7 @@
+let name = "sahil";
+let age = 19;
+let college = "DAV Institute of Technology";
+
+console.log(`My name is ${name}`);
+console.log("I am",age,"years old.");
+console.log("I study at " + collage);
