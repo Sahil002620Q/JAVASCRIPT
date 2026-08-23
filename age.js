d8e0980a518557dc44cf9c34208d4032c8f2c008
Sahil002620Q <smax06388@gmail.com>
2026-08-23 09:04:10 +0530

    different types to use var in console.log

---
+++ age.js
@@ -0,0 +1,7 @@
+let name = "sahil";
+let age = 19;
+let nxtage = age + 1;
+
+console.log("name :",name);
+console.log("age :",age);
+console.log("Next year i'll be",nxtage + " years old."); //auto space when using , and using , num bhave normally while + make it string like behaviour
